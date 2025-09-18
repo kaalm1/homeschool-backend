da0f50a94530da99e614259840c2b67b6efbe8be
Joe <joe@Joes-MacBook-Air.local>
2025-09-18 19:43:33 -0400

    add migration

--- svc/app/datatypes/user_behavior_analytic.py
+++ svc/app/datatypes/user_behavior_analytic.py
@@ -4,6 +4,7 @@ from typing import Any, Dict, List, Optional
 from pydantic import BaseModel, Field
 
 from svc.app.datatypes.enums import RepetitionTolerance
+from svc.app.datatypes.weather import WeatherDay
 
 
 class WeeklyContext(BaseModel):
